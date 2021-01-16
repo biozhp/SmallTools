3accaed4eb863ced0b1e0000465e2563fbdde7c1
Peng Zhao <biozhp@163.com>
2021-01-16 11:53:03 +0800

    Update

---
+++ eQTL/PCA.sh
@@ -0,0 +1,5 @@
+## results in eigenvec
+plink --allow-extra-chr --out temp --recode --vcf all.impute.385.m8f5.num.vcf
+plink --allow-extra-chr --file temp --noweb --make-bed --out m8f5
+./gcta64 --bfile ../m8f5 --make-grm --autosome --out tmp
+./gcta64 --grm tmp --pca --out m8f5.pca
